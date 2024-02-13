974ea6ea0b9d35b188186724e97f068e13b547fe
m_bubela <m_bubela@mifprojects.com>
2024-02-13 20:03:08 +0300

    Added profile files for mapping entity/dtos

---
+++ FeedbackAnalysisAI.BLL/MappingProfiles/FeedbackProfileBLL.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FeedbackAnalysisAI.Contracts.DTO;
+using FeedbackAnalysisAI.DAL.Entities;
+
+namespace FeedbackAnalysisAI.BLL.MappingProfiles
+{
+    public class FeedbackProfileBLL : Profile
+    {
+        public FeedbackProfileBLL()
+        {
+            #region To DTO
+            CreateMap<Feedback, FeedbackDTO>()
+                .ForMember(dest => dest.SentimentName, opt => opt.MapFrom(src => src.Sentiment.Name));
+            #endregion
+
+            #region from DTO
+            CreateMap<FeedbackDTO, Feedback>();
+            #endregion
+        }
+    }
+}
---
+++ FeedbackAnalysisAI.BLL/MappingProfiles/UserProfileBLL.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using FeedbackAnalysisAI.Contracts.DTO;
+using FeedbackAnalysisAI.DAL.Entities;
+
+namespace FeedbackAnalysisAI.BLL.MappingProfiles
+{
+    public class UserProfileBLL : Profile
+    {
+        public UserProfileBLL()
+        {
+            #region To DTO
+            CreateMap<User, UserDTO>();
+            #endregion
+
+            #region from DTO
+            CreateMap<UserDTO, User>();
+            #endregion
+        }
+    }
+}
