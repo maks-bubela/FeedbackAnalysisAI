64750a3296563549918d5d5705423d20a80de8f3
m_bubela <m_bubela@mifprojects.com>
2024-02-13 20:06:35 +0300

    Added jwt token to project

---
+++ FeedbackAnalysisAI.API/Interfaces/IAuthOptions.cs
@@ -0,0 +1,10 @@
+﻿using FeedbackAnalysisAI.Contracts.DTO;
+
+
+namespace FeedbackAnalysisAI.API.Interfaces
+{
+    public interface IAuthOptions
+    {
+        string GetSymmetricSecurityKey(TokenSettingsDTO settingsDto);
+    }
+}
