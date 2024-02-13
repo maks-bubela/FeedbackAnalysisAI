010d8da378ef2342f540623c60c518a6735d6c7c
m_bubela <m_bubela@mifprojects.com>
2024-02-13 19:59:40 +0300

    Added db context class

---
+++ FeedbackAnalysisAI.DAL/Context/FeedbackAnalysisAIContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FeedbackAnalysisAI.DAL.Entities;
+using FeedbackAnalysisAI.DAL.Seeder;
+using System.Reflection;
+
+namespace FeedbackAnalysisAI.DAL.Context
+{
+    public class FeedbackAnalysisAIContext : DbContext
+    {
+        public FeedbackAnalysisAIContext(string connectionString) : base(GetOptions(connectionString)) { }
+        public FeedbackAnalysisAIContext(DbContextOptions<FeedbackAnalysisAIContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Sentiment> Sentiments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<BearerTokenSetting> BearerTokenSettings { get; set; }
+        public DbSet<EnvironmentType> EnvironmentTypes { get; set; }
+
+
+        private static DbContextOptions<FeedbackAnalysisAIContext> GetOptions(string connectionString)
+        {
+            return new DbContextOptionsBuilder<FeedbackAnalysisAIContext>()
+                .UseNpgsql(connectionString)  
+                .Options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            DatabaseSeeder.SeedDataBase(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
