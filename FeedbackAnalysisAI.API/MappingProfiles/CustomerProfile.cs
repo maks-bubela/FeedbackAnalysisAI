52b98245873a2aed18ee54536715b1a0963fb08f
m_bubela <m_bubela@mifprojects.com>
2024-02-13 20:07:12 +0300

    Added mapping profilers

---
+++ FeedbackAnalysisAI.API/MappingProfiles/CustomerProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FeedbackAnalysisAI.Contracts.DTO;
+using FeedbackAnalysisAI.Contracts.Models;
+
+namespace FeedbackAnalysisAI.API.MappingProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            #region To Model
+            CreateMap<UserDTO, UserLoginModel>();
+            CreateMap<UserDTO, UserRegistrationModel>();
+            #endregion
+
+            #region To DTO
+            CreateMap<UserLoginModel, UserDTO>();
+            CreateMap<UserRegistrationModel, UserDTO>();
+            #endregion
+        }
+    }
+}
---
+++ FeedbackAnalysisAI.API/MappingProfiles/FeedbackProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FeedbackAnalysisAI.Contracts.DTO;
+using FeedbackAnalysisAI.Contracts.Models;
+
+namespace FeedbackAnalysisAI.API.MappingProfiles
+{
+    public class FeedbackProfile : Profile
+    {
+        public FeedbackProfile()
+        {
+            #region To Model
+            CreateMap<FeedbackDTO, FeedbackCreateModel>();
+            CreateMap<FeedbackDTO, FeedbackAndSentiemntModel>();
+            #endregion
+
+            #region To DTO
+            CreateMap<FeedbackCreateModel, FeedbackDTO>();
+            CreateMap<FeedbackAndSentiemntModel, FeedbackDTO>();
+            #endregion
+        }
+    }
+}
