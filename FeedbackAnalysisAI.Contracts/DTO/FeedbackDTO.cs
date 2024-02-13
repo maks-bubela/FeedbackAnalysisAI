06151b716560f42323037e4caa19e8108591293c
m_bubela <m_bubela@mifprojects.com>
2024-02-13 20:01:59 +0300

    Added DTOs classes

---
+++ FeedbackAnalysisAI.Contracts/DTO/FeedbackDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FeedbackAnalysisAI.Contracts.DTO
+{
+    public class FeedbackDTO
+    {
+        public string Text { get; set; }
+        public string? SentimentName { get; set; }
+        public long UserId { get; set; }
+    }
+}
---
+++ FeedbackAnalysisAI.Contracts/DTO/TokenSettingsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace FeedbackAnalysisAI.Contracts.DTO
+{
+    public class TokenSettingsDTO
+    {
+        public ClaimsIdentity Identity { get; set; }
+        public int LifeTime { get; set; }
+    }
+}
