52595600a9fa225991da2f26f59dfd527d1ff932
m_bubela <m_bubela@mifprojects.com>
2024-02-13 20:02:12 +0300

    Added some const

---
+++ FeedbackAnalysisAI.Contracts/Consts/TokenConsts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackAnalysisAI.Contracts.Consts
+{
+    public static class TokenConsts
+    {
+        public const string CacheTokenAccess = "accessToken";
+        public const string TokenKey = "Token";
+    }
+}
