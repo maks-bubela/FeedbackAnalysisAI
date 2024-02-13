9f85077d64b9217f47c9e41e51e47d38956bf123
m_bubela <m_bubela@mifprojects.com>
2024-02-13 20:04:12 +0300

    Added provider that works with feedbacks

---
+++ FeedbackAnalysisAI.AzureService/AiService/Provider/AzureAiServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using FeedbackAnalysisAI.Contracts.Services;
+
+namespace FeedbackAnalysisAI.AzureService.AiService.Provider
+{
+    public class AzureAiServiceProvider : IAzureAiServiceProvider
+    {
+        private readonly TextAnalyticsClient _textAnalyticsClient;
+
+        public AzureAiServiceProvider(Uri endpoint, AzureKeyCredential apiKey)
+        {
+            _textAnalyticsClient = new TextAnalyticsClient(endpoint, apiKey);
+        }
+
+        public async Task<TextSentiment> AnalyzeSentimentAsync(string feedbackText)
+        {
+
+            var response = await _textAnalyticsClient.AnalyzeSentimentAsync(feedbackText);
+
+            return response.Value.Sentiment;
+        }
+    }
+}
