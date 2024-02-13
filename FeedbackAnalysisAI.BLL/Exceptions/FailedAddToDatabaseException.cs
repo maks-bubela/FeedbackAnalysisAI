fd52dc090f17991bd4457eb542488194a22889ca
m_bubela <m_bubela@mifprojects.com>
2024-02-13 20:03:20 +0300

    Added custom exception

---
+++ FeedbackAnalysisAI.BLL/Exceptions/FailedAddToDatabaseException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FeedbackAnalysisAI.BLL.Exceptions
+{
+    public class FailedAddToDatabaseException : Exception
+    {
+        private const string FailedAdd = "Failed add to database. Please try do this later.";
+
+        public FailedAddToDatabaseException()
+        : base(FailedAdd)
+        { }
+    }
+}
---
+++ FeedbackAnalysisAI.BLL/Exceptions/NotFoundInDatabaseException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FeedbackAnalysisAI.BLL.Exceptions
+{
+    public class NotFoundInDatabaseException : Exception
+    {
+        private const string ExceptionText = "Data wasn't found in database.";
+
+        public NotFoundInDatabaseException()
+        : base(ExceptionText)
+        { }
+    }
+}
